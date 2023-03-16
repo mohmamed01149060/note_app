fa84f0e3eeaf05d2006353c57040941cecbe3c75
woahmedosmaa <mohamedmedo9932@gmail.com>
2023-03-16 14:45:26 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(NoteApp());
+}
+
+class NoteApp extends StatelessWidget {
+  const NoteApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
