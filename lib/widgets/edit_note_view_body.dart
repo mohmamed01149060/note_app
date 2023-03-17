995ec89be1b6d284c30d4f7a5e9cf70514de62b3
woahmedosmaa <mohamedmedo9932@gmail.com>
2023-03-17 02:06:02 +0200

    add Edit Note Page

--- lib/widgets/edit_note_view_body.dart
+++ lib/widgets/edit_note_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:note_app/widgets/custom_app_bar.dart';
+import 'package:note_app/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({Key? key}) : super(key: key);
@@ -15,7 +16,14 @@ class EditNoteViewBody extends StatelessWidget {
             CustomAppBar(
               icon: Icon(Icons.check),
               text: 'Edit Note',
-            )
+            ),
+            SizedBox(height: 30),
+            CustomTextField(hintText: 'Title'),
+            SizedBox(height: 16),
+            CustomTextField(
+              hintText: 'Content',
+              maxLines: 5,
+            ),
           ],
         ),
       ),
