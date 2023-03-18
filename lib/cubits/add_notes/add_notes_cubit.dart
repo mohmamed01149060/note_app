b268e1fd881cb22c9ca469cb2327556cd3e8e640
woahmedosmaa <mohamedmedo9932@gmail.com>
2023-03-18 17:57:47 +0200

    add note method (logic) about states (loading,success,failure) in add note cubit

--- lib/cubits/add_notes/add_notes_cubit.dart
+++ lib/cubits/add_notes/add_notes_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
+import 'package:note_app/constants.dart';
 import 'package:note_app/models/note_model.dart';
 
 part 'add_notes_state.dart';
@@ -7,5 +9,18 @@ part 'add_notes_state.dart';
 class AddNotesCubit extends Cubit<AddNotesState> {
   AddNotesCubit() : super(AddNotesInitial());
 
-  addNotes(NoteModel note) {}
+  addNotes(NoteModel note) async {
+    emit(AddNotesLoading());
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      emit(AddNotesSuccess());
+      await notesBox.add(note);
+    } catch (e) {
+      emit(
+        AddNotesFailure(
+          messageError: e.toString(),
+        ),
+      );
+    }
+  }
 }
